b14bd285bef0964742b029514bda4e83992f5910
Musab Khan <godxxillaa@gmail.com>
2024-11-26 03:19:49 +0400

    Just wrote about data types in here.

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,14 +1,14 @@
 "use strict"; // This treat all JS code as newer version.
 
-alert("Hello") // We are using node JS not web browser
+// alert("Hello") // We are using node JS not web browser
 
-console.log(3 + 3); console.log("Hetaish"); // Don't fuck the readablity :).
+// console.log(3 + 3); console.log("Hetaish"); // Don't fuck the readablity :).
 
 // Code readablity is a priority.
 
 
 let name = "Musab"
-let age = "23"
+let age = 23
 let isLoggedIn = false
 
 // number => 2 to the power 53.
@@ -19,3 +19,6 @@ let isLoggedIn = false
 // undefined => 
 // Symbol => 
 // Object => 
+
+
+console.log(typeof age);
