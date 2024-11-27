52e20c93c4c4156712014007a3aa1097e40945a6
Musab Khan <godxxillaa@gmail.com>
2024-11-27 23:27:45 +0400

    03_conversionOperation file code added

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,22 @@
-let score = 23;
+// let score = 10 ** 5 ;
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = BigInt(score)
 
-console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
+// console.log(typeof(valueInNumber));
+
+// let isLoggedIn = "2";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 50;
+
+let stringSomeNumber = String(someNumber);
+
+console.log(stringSomeNumber);
+console.log(typeof stringSomeNumber);
