132c2888141e5232a283a13607a4082c9faa5e7b
Musab Khan <godxxillaa@gmail.com>
2024-11-29 07:00:10 +0400

    Just practiced more

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,9 +14,60 @@
 // console.log(booleanIsLoggedIn);
 
 
-let someNumber = 50;
+// let someNumber = 50;
 
-let stringSomeNumber = String(someNumber);
+// let stringSomeNumber = String(someNumber);
 
-console.log(stringSomeNumber);
-console.log(typeof stringSomeNumber);
+// console.log(stringSomeNumber);
+// console.log(typeof stringSomeNumber);
+
+
+// ###################  Operations ###########################
+
+// let value = 3;
+
+// let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+// let str1 = "Hello"
+// let str2 = " Musab"
+
+// let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+"");
+
+// let num1, num2, num3;
+
+// num1 = num2 = num3 = 2 + 2;
+
+// console.log(num1);
+
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+// let a = 3; The prefix one change the main variable
+// const a = 3; id we try to make it a const i will through an error
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
